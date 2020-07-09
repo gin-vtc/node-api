8b59ad9d73cfb89aa7042b45b75d9723444385b5
ギン <197370556@stc.vtc.edu.hk>
2020-07-10 02:37:13 +0800

    Bug Fix

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const dbFunc = require('./func');
 const app = express();
 const port = 3000
 
+//　デフォルト
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 //　ルート
 app.get('/', (req, res) => {
     // ステータスチェック
@@ -121,7 +141,7 @@ app.get('/login', (req, res) => {
         // res.sendStatus(200)
         for (let x in userDB) {
             if (userDB[x].user === user && userDB[x].password === password) {
-                status = 200
+                res.status(200).json(userDB[x])
             }
         }
         res.sendStatus(status)
