d394bb7f77b62e77f3d541054d03ebb04bb7351e
ギン <197370556@stc.vtc.edu.hk>
2020-07-10 00:54:27 +0800

    User API

---
+++ func.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+
+module.exports = {
+    statusMsg: function (status) {
+        const obj = {
+            status
+        }
+        return JSON.stringify(obj)
+    },
+    makeid:(length)=>{
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    },
+    checkId:(dbFile,id)=>{
+        let db = fs.readFileSync('db/' + dbFile + '.json');
+        let dbData = JSON.parse(db);
+        for (const x in dbData){
+            if(data.id === id){
+                return false
+            }
+        }
+        return true
+    },
+    getJson: function (dbFile) {
+        try {
+            let db = fs.readFileSync('db/' + dbFile + '.json');
+            let data = JSON.parse(db);
+            return data;
+        } catch (err) {
+            return false
+        }
+        // whatever
+    },
+    updateDb: function (dbFile, data) {
+        try {
+            const json = JSON.stringify(data)
+            fs.writeFileSync('db/' + dbFile + '.json', json);
+            return 200;
+        } catch (err) {
+            console.log(err)
+            return 500
+        }
+    },
+    appendJson: function (dbFile, data) {
+        try {
+            let db = fs.readFileSync('db/' + dbFile + '.json');
+            let dbData = JSON.parse(db);
+            for (const x in dbData){
+                if(data.user === dbData[x].user){
+                    return 400
+                }
+            }
+            dbData.push(data)
+            const json = JSON.stringify(dbData)
+            fs.writeFileSync('db/' + dbFile + '.json', json);
+
+            return 200;
+        } catch (err) {
+            console.log(err)
+            return 500
+        }
+    }
+};
--- index.js
+++ index.js
@@ -1,12 +1,88 @@
 const express = require('express');
+const dbFunc = require('./func');
 const app = express();
 const port = 3000
 
-app.get('/',(req, res) => {
-    res.send('Hello World!');
+//　ルート
+app.get('/', (req, res) => {
+    // ステータスチェック
+    res.sendStatus(200)
+    res.send('ok');
+});
+
+// ログイン
+app.get('/login', (req, res) => {
+    const {user, password} = req.query
+    userDB = dbFunc.getJson('user')
+    console.log(user)
+    if (user) {
+        let status = 400
+        // res.sendStatus(200)
+        for (let x in userDB) {
+            if (userDB[x].user === user && userDB[x].password === password) {
+                status = 200
+            }
+        }
+        res.sendStatus(status)
+    } else {
+        res.sendStatus(500)
+    }
+});
+app.get('/user', (req, res) => {
+    const {id} = req.query
+    userDB = dbFunc.getJson('user')
+    let status = 404
+    for (x in userDB) {
+        console.log(userDB[x].id, id)
+        if (userDB[x].id === id) {
+            status = 200
+        }
+    }
+    console.log(status)
+    if (status === 404) {
+        res.send(404)
+    } else {
+        res.status(200).json(userDB[x])
+    }
+});
+app.post('/user', (req, res) => {
+    const {user, password, name} = req.query
+
+    let id = ''
+    do {
+        id = dbFunc.makeid(15)
+    } while (dbFunc.makeid('user', id));
+    //　同じIDを回避する
+
+
+    if (user && password && name) {
+        const status = dbFunc.appendJson('user', {id, user, password, name})
+        res.sendStatus(status)
+    } else {
+        res.sendStatus(401)
+    }
+});
+app.put('/user', (req, res) => {
+    const {user, password, newPwd, newName} = req.query
+    userDB = dbFunc.getJson('user')
+    for (x in userDB) {
+        if (userDB[x].user === user && userDB[x].password === password) {
+            if(newPwd){
+                userDB[x].password = newPwd
+            }
+            if(newName){
+                userDB[x].name = newName
+            }
+           const status =  dbFunc.updateDb('user',userDB)
+            res.sendStatus(status)
+        }
+    }
+    res.sendStatus(401)
+
 });
 
 
+//　サバー起動
 app.listen(port, () => {
     console.log(`APIサーバーを起動します(Port: ${port})`);
 });
